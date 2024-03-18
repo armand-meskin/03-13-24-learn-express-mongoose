911da07dcea1bb79e8e3dc24c0fd47d07de51e4e
armand-meskin <armandm101@gmail.com>
2024-03-18 16:25:18 -0400

    Completed

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,17 @@
 let BookInstance = require('../models/bookinstance');
+let Book = require('../models/book');
+const bookinstance = require('../models/bookinstance');
+
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  
+  bookinstance.find({'status': {$eq: 'Available'}})
+  .populate('book')
+  .exec()
+  .then(list_bookinstances => {
+    res.send(list_bookinstances.map(function(bookInstance) {
+      return bookInstance.book.title +  " : " + bookInstance.status;
+    }));
+  })
+  .catch(err => res.send('Status not found'));
 }
